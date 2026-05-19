22764db82a55bb2fe747a2ca1107734860b3051c
Darshan1124 <darshanthummar9429@gmail.com>
2026-05-19 20:27:03 +0100

    cron job added in system

---
+++ src/main/java/com/example/journal/service/CounterJobService.java
@@ -0,0 +1,22 @@
+package com.example.journal.service;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Service
+public class CounterJobService {
+    private final AtomicInteger counter = new AtomicInteger(0);
+
+    @Scheduled(cron = "*/10 * * * * *")
+    public void IncrementCounter(){
+        int val=counter.incrementAndGet();
+        System.out.println(val);
+    }
+
+    public int getCounter() {
+        return counter.get();
+    }
+
+}
